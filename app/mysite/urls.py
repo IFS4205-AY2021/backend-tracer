54d404c4c89297fe07a4c9e2604bfe7e02c9dbbb
Grandchunwei <scengka@gmail.com>
2020-10-05 07:30:10 +0800

    update tracer

--- app/mysite/urls.py
+++ app/mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tracer/', include('subsys_tracer.urls'))
+    path('api/tracer/', include('subsys_tracer.urls'))
 ]
--- app/subsys_tracer/admin.py
+++ app/subsys_tracer/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.User)
+admin.site.register(models.StayHomeRecord)
+admin.site.register(models.Location)
+admin.site.register(models.Admin)
+admin.site.register(models.Researcher)
+admin.site.register(models.Tracer)
+admin.site.register(models.Contact)
+admin.site.register(models.Record)
